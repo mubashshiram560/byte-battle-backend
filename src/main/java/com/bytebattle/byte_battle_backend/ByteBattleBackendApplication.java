9bd0d56bbf202c7163f4a70a73b462293dffd6c4
mubashshira560 <mubashshiram560@gmail.com>
2026-09-03 13:08:21 +0530

    Initial Spring Boot project setup

---
+++ src/main/java/com/bytebattle/byte_battle_backend/ByteBattleBackendApplication.java
@@ -0,0 +1,13 @@
+package com.bytebattle.byte_battle_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ByteBattleBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ByteBattleBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bytebattle/byte_battle_backend/ByteBattleBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bytebattle.byte_battle_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ByteBattleBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
